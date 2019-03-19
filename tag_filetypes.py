e16970f6271d3117e6ece284230022faf0e540b5
Jim Gu <heming.gu@mercurygate.com>
2019-03-19 13:34:02 -0400

    add basic implementation of asyncio

--- tag_filetypes.py
+++ tag_filetypes.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
 import argparse
+import asyncio
 import logging
 import os
 import re
 
+import aiobotocore
 import boto3
 import botocore
 
@@ -22,11 +24,12 @@ class S3Helper():
 
     def __init__(self, bucket):
         self.s3 = boto3.resource('s3')
-        self.s3_client = boto3.client('s3')
+        self.session = aiobotocore.get_session()
+        self.s3_client = self.session.create_client('s3')
         self.bucket = self.s3.Bucket(name=bucket)
 
-    def update_object_tag(self, obj, key, value):
-        response = self.s3_client.get_object_tagging(
+    async def update_object_tag(self, obj, key, value):
+        response = await self.s3_client.get_object_tagging(
             Bucket=self.bucket.name,
             Key=obj.key,
         )
@@ -35,7 +38,7 @@ class S3Helper():
         if tags.get(key) != value:
             changed = True
             tags[key] = value
-            response = self.s3_client.put_object_tagging(
+            response = await self.s3_client.put_object_tagging(
                 Bucket=self.bucket.name,
                 Key=obj.key,
                 Tagging=dict(
@@ -46,26 +49,35 @@ class S3Helper():
         return changed, tags
 
 
-def main():
+async def main():
     args = get_args()
 
     s3_helper = S3Helper(bucket=args.bucket)
+
+    tasks = list()
 
     found_objects = False
     for s3_obj in s3_helper.bucket.objects.filter(Prefix=args.prefix):
         found_objects = True
         filetype = os.path.splitext(s3_obj.key)[1].strip('.').lower()
         print(s3_obj.key)
-        try:
-            s3_helper.update_object_tag(s3_obj, 'filetype', filetype)
-        except botocore.exceptions.ClientError as e:
-            import pdb; pdb.set_trace()
-            print('Error on key %s: %s' % (s3_obj.key, e.msg))
-            continue
+        tasks.append(
+            asyncio.create_task(
+                s3_helper.update_object_tag(s3_obj, 'filetype', filetype)
+            )
+        )
+        #except botocore.exceptions.ClientError as e:
+        #    import pdb; pdb.set_trace()
+        #    print(e)
+        #    print('Error on key %s: %s' % (s3_obj.key, e.msg))
+        #    continue
 
     if not found_objects:
         raise IOError('No objects with prefix %s found!' % args.prefix)
 
+    for task in tasks:
+        await task
+
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
